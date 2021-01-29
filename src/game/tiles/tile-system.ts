b1d74c489f2947e0f20c6d5841ff05cfc91e49ba
maqqr <maqqr@users.noreply.github.com>
2021-01-29 21:51:09 +0200

    Optimized TileSystem a bit

--- src/game/tiles/tile-system.ts
+++ src/game/tiles/tile-system.ts
@@ -3,14 +3,21 @@ import { Position, Velocity } from '../components'
 import { getPrioritySystems, registerWithPriority } from '../../register-system'
 import { Game } from '../constants'
 import { Coordinate } from '../coordinate-system/coordinate'
+import { Tile } from './tile'
 
 @registerWithPriority(1)
 class TileSystem extends System {
 
-    private tileCache: { [id: string]: Entity }
+    private tileCache: { [id: string]: Entity } | null = null
 
     static queries = {
-        tiles: { components: [Coordinate] }
+        tiles: {
+            components: [Coordinate, Tile],
+            listen: {
+                added: true,
+                removed: true
+            }
+        }
     }
 
     private positionHash(coord: Coordinate): string {
@@ -18,13 +25,30 @@ class TileSystem extends System {
     }
 
     execute(deltaTime: number, time: number) {
-        this.queries.tiles.results.forEach(entity => {
-            const coord = entity.getComponent(Coordinate)!
-            this.tileCache[this.positionHash(coord)] = entity
+        // Invalidate tile cache when tile entities are created/removed
+        this.queries.tiles.added!.forEach(entity => {
+            this.tileCache = null
         })
+        this.queries.tiles.removed!.forEach(entity => {
+            this.tileCache = null
+        })
+
+        // Rebuild tile cache if needed
+        if (!this.tileCache) {
+            this.tileCache = {}
+            this.queries.tiles.results.forEach(entity => {
+                const coord = entity.getComponent(Coordinate)!
+                this.tileCache![this.positionHash(coord)] = entity
+            })
+        }
     }
 
     getTileAt(coord: Coordinate): Entity | null {
-        return this.tileCache[this.positionHash(coord)]
+        if (!this.tileCache) {
+            console.error("Tried to access tile position before TileSystem.tileCache was refreshed")
+            return null
+        } else {
+            return this.tileCache[this.positionHash(coord)]
+        }
     }
 }
