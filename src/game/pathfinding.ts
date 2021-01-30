154bb3b15d58242a2bcdb12bb4b5f68ae1b97f90
maqqr <maqqr@users.noreply.github.com>
2021-01-30 23:52:00 +0200

    Building have field of view, small pathfind fixes

--- src/game/pathfinding.ts
+++ src/game/pathfinding.ts
@@ -1,37 +1,43 @@
 import { Coordinate } from "./coordinate-system/coordinate"
-import { coordinateToXY, equalCoordinates, getDistance, getNeighbourCoordinates } from "./coordinate-system/omnipotent-coordinates" 
+import { coordinateToXY, equalCoordinates, getDistance, getNeighbourCoordinates } from "./coordinate-system/omnipotent-coordinates"
 
 
 
-export function pathfind(start: Coordinate, end: Coordinate, passableCallback: (pos: Coordinate) => Boolean): Coordinate[] {
-    //return aStarPathfind(start, end, passableCallback)
+export function pathfind(start: Coordinate, end: Coordinate,
+                         passableCallback: (pos: Coordinate) => boolean): Coordinate[] {
     return breadthFirstPathfind(start, end, passableCallback)
 }
 
-function breadthFirstPathfind(start: Coordinate, end: Coordinate, passableCallback: (pos: Coordinate) => Boolean): Coordinate[]{
+function breadthFirstPathfind(start: Coordinate, end: Coordinate,
+                              passableCallback: (pos: Coordinate) => boolean): Coordinate[] {
     if (start === undefined || end === undefined) {
         console.error("pathfind start or end if undefined")
     }
 
     const frontier: Coordinate[] = [start]
     const cameFrom : {[id: string] : Coordinate} = {}
-    
+
+    let foundPath = false
+
     // the loop starts with first coordinate by default
     while (frontier.length > 0) {
         const newAnalysisCoordinate = frontier[0]
         frontier.splice(0, 1)
-        if (equalCoordinates(newAnalysisCoordinate, end)){
+        if (equalCoordinates(newAnalysisCoordinate, end)) {
+            foundPath = true
             break
         }
-        const coords : Coordinate[] = checkNeighbourCoordinates(newAnalysisCoordinate, passableCallback)
-        for (let index = 0; index < coords.length; index++) {
-            const next = coords[index]
+        for (const next of checkNeighbourCoordinates(newAnalysisCoordinate, passableCallback)) {
             const key : string = "" + next.x + "," + next.y + "," + next.z
             if (!(key in cameFrom)){
                 frontier.push(next)
                 cameFrom[key] = newAnalysisCoordinate
             }
         }
+    }
+
+    if (!foundPath) {
+        return []
     }
 
     let current : Coordinate = end
@@ -48,7 +54,8 @@ function breadthFirstPathfind(start: Coordinate, end: Coordinate, passableCallba
     return path.reverse()
 }
 
-function aStarPathfind(start: Coordinate, end: Coordinate, passableCallback: (pos: Coordinate) => Boolean): Coordinate[] {
+function aStarPathfind(start: Coordinate, end: Coordinate,
+                       passableCallback: (pos: Coordinate) => boolean): Coordinate[] {
     const frontier: Coordinate[] = [start]
     const visited: Coordinate[] = []
     const path: Coordinate[] = []
@@ -76,19 +83,18 @@ function aStarPathfind(start: Coordinate, end: Coordinate, passableCallback: (po
             }
             visited.push(frontier[index])
         }
-    } 
-    */   
+    }
+    */
     return []
 }
 
-export function checkNeighbourCoordinates(origin: Coordinate, passableCallback: (pos: Coordinate) => Boolean)
-    : Coordinate[]{
+export function checkNeighbourCoordinates(origin: Coordinate,
+                                          passableCallback: (pos: Coordinate) => boolean) : Coordinate[] {
     const result : Coordinate[] = []
 
-    const unfilteredNeighbors : Coordinate[] =  getNeighbourCoordinates(origin)
+    const unfilteredNeighbors : Coordinate[] = getNeighbourCoordinates(origin)
 
-    for (let index = 0; index < unfilteredNeighbors.length; index++) {
-        const element = unfilteredNeighbors[index]
+    for (const element of unfilteredNeighbors) {
         if (passableCallback(element)){
             result.push(element)
         }
